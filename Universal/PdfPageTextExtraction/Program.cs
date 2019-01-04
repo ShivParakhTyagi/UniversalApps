ab5ee5968e4a645b2978f95e51cda8cc85da6446
Shiv Tyagi <tyagi_shivparakh00@live.com>
2019-01-04 13:13:01 +0530

    poc

---
+++ Universal/PdfPageTextExtraction/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Syncfusion.Pdf;
+using Syncfusion.Pdf.Parsing;
+
+namespace PdfPageTextExtraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var pdf =
+                new PdfLoadedDocument(
+                    @"C:\Users\shiv\GitHub\Source\Repos\UniversalApps\Universal\PdfPageTextExtraction\Dump\test.pdf"))
+            {
+                foreach (PdfPageBase page in pdf.Pages)
+                {
+                    page.Rotation = PdfPageRotateAngle.RotateAngle90;
+                    List<TextData> textDataCollection;
+                    string extractedText = page.ExtractText(out textDataCollection);
+                    page.Rotation = PdfPageRotateAngle.RotateAngle0;
+                    List<TextData> textDataCollection1;
+                    string extractedText1 = page.ExtractText(out textDataCollection1);
+
+                    var t = textDataCollection.FirstOrDefault(x => x.Text.Contains("CUSTOMER"));
+                    var t1 = textDataCollection1.FirstOrDefault(x => x.Text.Contains("CUSTOMER"));
+                }
+
+                pdf.Save();
+            }
+        }
+    }
+}
